ab5b9fedd256c8b4e362b6f8bbcbd5756fad9c1e
Kurlgargyey <nahsirkivar@gmail.com>
2022-03-31 19:10:46 +0200

    rubocop compliance

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
+# this class keeps score and handles the moves it's being passed by the Players
 class Game
-  
-  def initialize()
+  def initialize
     @scores = Hash.new(0)
-    self.board = Array.new(3){Array.new(3)}
-    puts "Please enter your name, Player 1:"
+    self.board = Array.new(3) { Array.new(3) }
+    puts 'Please enter your name, Player 1:'
     @player1 = Player.new(gets.chomp)
-    puts "Please enter your name, Player 2:"
+    puts 'Please enter your name, Player 2:'
     @player2 = Player.new(gets.chomp)
   end
 
@@ -18,104 +18,112 @@ class Game
     until turn == 10
       puts "Turn #{turn}:"
       process_turn(active_player, passive_player)
-      if active_player.winner?()
-        break
-      end
-      turn +=1
+      break if active_player.winner?
+
+      turn += 1
       active_player, passive_player = passive_player, active_player
     end
-    if turn == 10
-      puts "There was no winner by turn 9."
-    end
-    @scores[active_player] += 1
-    puts "Congratulations! #{active_player.name} won!"
-    puts "The score is #{@scores[active_player]} for #{active_player.name} and #{@scores[passive_player]} for #{passive_player.name}."
-    self.reinitialize
+    game_over(turn, active_player, passive_player)
   end
 
   protected
+
   attr_accessor :board
 
-  def process_turn (active_player,passive_player)
+  def draw_move(player, move)
+    case player
+    when @player1
+      board[move[0] - 1][move[1] - 1] = 'X'
+    when @player2
+      board[move[0] - 1][move[1] - 1] = 'O'
+    end
+  end
+
+  def process_turn(active_player,passive_player)
     puts "It's #{active_player.name}'s turn! Pick a field!"
     move = active_player.make_move(passive_player)
-    case active_player
-    when @player1
-      self.board[move[0]-1][move[1]-1] = 'X'
-    when @player2
-      self.board[move[0]-1][move[1]-1] = 'O'
-    end
-    active_player.moves.push(active_player.class.points_map[move[0]-1][move[1]-1])
+    draw_move(active_player, move)
+    active_player.moves.push(active_player.points_map[move[0] - 1][move[1] - 1])
+  end
+
+  def clear_game
+    @player1.moves.clear
+    @player2.moves.clear
+    self.board = Array.new(3){ Array.new(3) }
   end
 
   def reinitialize
-    puts "Do you want to play again? Y/N"
+    puts 'Do you want to play again? Y/N'
     ans = gets.chomp
-    if ans.casecmp('y') == 0
-      @player1.moves.clear
-      @player2.moves.clear
-      self.board = Array.new(3){Array.new(3)}
+    sleep 2
+    if ans.casecmp('y').zero?
+      clear_game
       puts "Okay! Let's go again!"
-      sleep 2
-      self.run_match
+      run_match
     else
-      sleep 2
-      puts "Alright! See you and have a great day!"
+      puts 'Alright! See you and have a great day!'
     end
+  end
+
+  def game_over(turn, winner, loser)
+    puts 'There was no winner by turn 9.' if turn == 10
+    @scores[winner] += 1
+    puts "Congratulations! #{winner.name} won!"
+    puts "The score is #{@scores[winner]} for #{winner.name} and #{@scores[loser]} for #{loser.name}."
+    reinitialize
   end
 end
 
+# this class takes moves from the player, validates them, and passes them to the Game class
 class Player
-  attr_reader :name
+  attr_reader :name, :points_map
   attr_accessor :moves
-  @@points_map = [
-    [8, 3, 4],
-    [1, 5, 9],
-    [6, 7, 2]
-  ]
 
   def initialize(name)
     @name = name
     @moves = []
+    @points_map = [
+      [8, 3, 4],
+      [1, 5, 9],
+      [6, 7, 2]
+    ]
   end
 
   def make_move(opponent)
-    move = get_move()
-    unless opponent.moves.include?(@@points_map[move[0]-1][move[1]-1]) || self.moves.include?(@@points_map[move[0]-1][move[1]-1])
-      return move
-    else
-      puts "That field has already been chosen. Choose again."
+    move = input_move
+    if opponent.check_moves(move) || check_moves(move)
+      puts 'That field has already been chosen. Choose again.'
       make_move(opponent)
+    else
+      move
     end
   end
 
-  def winner?()
-    self.moves.combination(3) do |triplet| 
-      if triplet.sum == 15
-        return true
-      end
+  def winner?
+    moves.combination(3) do |triplet|
+      return true if triplet.sum == 15
     end
     false
   end
 
   protected
 
-  def self.points_map
-    @@points_map
+  def check_moves(move)
+    moves.include?(self.points_map[move[0] - 1][move[1] - 1])
   end
 
   private
-  def get_move
-    input = +""
-    while !/^[1-3],\s[1-3]$/.match(input)
-      puts "Please select a field (enter its board coordinates in the format \"x, y\"):"
+
+  def input_move
+    input = +''
+    until /^[1-3],\s[1-3]$/.match(input)
+      puts 'Please select a field (enter its board coordinates in the format \"x, y\"):'
       input = gets.chomp
     end
-    move = input.split(", ")
-    move.map {|e| e.to_i}
+    move = input.split(', ')
+    move.map(&:to_i)
   end
-  
 end
 
-match = Game.new()
+match = Game.new
 match.run_match
